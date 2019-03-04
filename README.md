404850367f751a8af0212ace3c3a8b96de9d8f1c
zileyuan <zileyuan@qq.com>
2019-03-04 13:35:13 +0800

    update readme , changeslog and license

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # flutter_walle
 
-A new flutter plugin of walle.
+Get channel information (by meituan walle) from within the Flutter application.
+
+# Usage
+
+Example:
+
+```dart
+import 'package:flutter_walle/flutter_walle.dart';
+
+String channel = 'Unknown';
+try {
+    channel = await FlutterWalle.channel;
+} catch (e) {
+    print(e.toString());
+}
+```
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_walle/flutter_walle.dart';
 
 void main() => runApp(MyApp());
@@ -17,22 +16,17 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    initChannelState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
+  Future<void> initChannelState() async {
     String channel;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       channel = await FlutterWalle.channel;
-    } on PlatformException {
-      channel = 'Failed to get channel.';
+    } catch (e) {
+      print(e.toString());
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     setState(() {
